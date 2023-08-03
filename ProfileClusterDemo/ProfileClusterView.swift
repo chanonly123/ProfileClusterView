8cb43928114c91b0d26adeb301b83e593b4dd380
Chandan Karmakar <chan.only.123@gmail.com>
2023-08-03 12:56:01 +0530

    justify alignment added

--- ProfileClusterDemo/ProfileClusterView.swift
+++ ProfileClusterDemo/ProfileClusterView.swift
@@ -8,29 +8,41 @@ import UIKit
 
 @IBDesignable
 public class ProfileClusterView: UIView {
-    let stackView = UIStackView()
-    var leadingConst: NSLayoutConstraint!
-    var trailingConst: NSLayoutConstraint!
-    var centerConst: NSLayoutConstraint!
+    public let stackView = UIStackView()
+    private var leadingConst: NSLayoutConstraint!
+    private var trailingConst: NSLayoutConstraint!
+    private var centerConst: NSLayoutConstraint!
 
     public var configureMoreView: (((label: UILabel, more: Int)) -> Void)?
     public var configureImageView: (((imageView: UIImageView, index: Int)) -> Void)?
+    
+    public var configureMoreViewCustom: (((view: UIView, more: Int)) -> Void)?
+    public var configureImageViewCustom: (((view: UIView, index: Int)) -> Void)?
+    
     public var configureCount: (() -> Int)?
+    public var reloadWhenLayout = true
+    public var itemWidth: CGFloat?
     
     // view cache to optimize resizing
-    var imageViewCache = [Int: UIView]()
+    private var imageViewCache = [Int: UIView]()
+    public var widthConst: NSLayoutConstraint!
     
     @IBInspectable
-    var shadowLevel: CGFloat = 0.0 {
+    public var shadowLevel: CGFloat = 0.0 {
         didSet {
             setShadowLevel(shadowLevel)
         }
     }
     
     @IBInspectable
-    var maxVisible: Int = 0 {
+    public var maxVisible: Int = 0 {
         didSet {
             reloadDataInternal()
+            if configureImageViewCustom == nil && configureMoreViewCustom == nil {
+                stackView.arrangedSubviews.forEach { sub in
+                    sub.subviews.first?.layer.cornerRadius = getItemWidth() / 2
+                }
+            }
         }
     }
 
@@ -39,7 +51,7 @@ public class ProfileClusterView: UIView {
         didSet { updateStackViewSize() }
     }
 
-    /// "left", "right", "center"
+    /// "left", "right", "center", "justify"
     @IBInspectable
     public var alignment: String = "left" {
         didSet { updateStackViewSize() }
@@ -66,7 +78,7 @@ public class ProfileClusterView: UIView {
         reloadData()
     }
 
-    func commonInit() {
+    private func commonInit() {
         stackView.axis = .horizontal
         stackView.distribution = .fillEqually
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -77,21 +89,37 @@ public class ProfileClusterView: UIView {
         centerConst = centerXAnchor.constraint(equalTo: stackView.centerXAnchor, constant: 0)
         topAnchor.constraint(equalTo: stackView.topAnchor, constant: 0).isActive = true
         stackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0).isActive = true
-
+        
         leadingConst.isActive = true
         trailingConst.isActive = true
         centerConst.isActive = true
+        
+        widthConst = widthAnchor.constraint(equalToConstant: 1)
+        widthConst.isActive = false
+        widthConst.priority = .defaultHigh
 
         updateStackViewSize()
     }
 
     override public func layoutSubviews() {
         super.layoutSubviews()
-        reloadDataInternal()
+        if reloadWhenLayout {
+            reloadDataInternal()
+        }
+        if configureImageViewCustom == nil && configureMoreViewCustom == nil {
+            stackView.arrangedSubviews.forEach { sub in
+                sub.subviews.first?.layer.cornerRadius = getItemWidth() / 2
+            }
+        }
+    }
+    
+    private func getItemWidth() -> CGFloat {
+        return itemWidth ?? bounds.height
     }
 
-    func updateStackViewSize() {
+    private func updateStackViewSize() {
         stackView.spacing = spacing
+        stackView.distribution = .fillEqually
 
         centerConst?.isActive = false
         leadingConst?.isActive = false
@@ -107,8 +135,16 @@ public class ProfileClusterView: UIView {
             trailingConst?.isActive = true
             leadingConst?.priority = .defaultLow
             trailingConst?.priority = .defaultHigh
-        } else {
+        } else if alignment == "center" {
             centerConst?.isActive = true
+        } else if alignment == "justify" {
+            leadingConst?.isActive = true
+            trailingConst?.isActive = true
+            leadingConst?.priority = .defaultHigh
+            trailingConst?.priority = .defaultHigh
+            stackView.distribution = .equalSpacing
+        } else {
+            assertionFailure("ProfileClusterView alignment value wrong")
         }
 
         let scaleX: CGFloat = startFrom == "left" ? 1 : -1
@@ -120,22 +156,29 @@ public class ProfileClusterView: UIView {
         imageViewCache.removeAll()
         prevBounds = bounds.insetBy(dx: 10, dy: 10)
         reloadDataInternal()
+        
+        if configureImageViewCustom == nil {
+            stackView.arrangedSubviews.forEach { sub in
+                sub.subviews.first?.layer.cornerRadius = getItemWidth() / 2
+            }
+        }
     }
 
-    var prevBounds = CGRect.zero
+    private var prevBounds = CGRect.zero
 
     private func reloadDataInternal() {
         if bounds == prevBounds { return }
         prevBounds = bounds
 
         let count = configureCount?() ?? 0
+        widthConst.constant = CGFloat(count) * getItemWidth() + (CGFloat(count - 1) * spacing)
 
-        let singleWidth = bounds.height + spacing
+        let singleWidth = getItemWidth() + spacing
         var visibleCount = 0
         if singleWidth > 0 {
-            visibleCount = Int((bounds.width - bounds.height) / singleWidth)
+            visibleCount = Int((bounds.width - getItemWidth()) / singleWidth)
         }
-        if bounds.width > bounds.height {
+        if bounds.width > getItemWidth() {
             visibleCount += 1
         } else {
             visibleCount = 1
@@ -164,7 +207,7 @@ public class ProfileClusterView: UIView {
         updateStackViewSize()
     }
 
-    func addImageView(index: Int) {
+    private func addImageView(index: Int) {
         if let found = imageViewCache[index] {
             stackView.addArrangedSubview(found)
             return
@@ -172,11 +215,20 @@ public class ProfileClusterView: UIView {
         let sub = UIView()
         imageViewCache[index] = sub
         sub.translatesAutoresizingMaskIntoConstraints = false
-        sub.heightAnchor.constraint(equalTo: sub.widthAnchor, multiplier: 1, constant: 0).isActive = true
         stackView.addArrangedSubview(sub)
+        if let itemWidth = itemWidth {
+            sub.heightAnchor.constraint(equalTo: stackView.heightAnchor, multiplier: 1, constant: 0).isActive = true
+            sub.widthAnchor.constraint(equalToConstant: itemWidth).isActive = true
+        } else {
+            sub.heightAnchor.constraint(equalTo: sub.widthAnchor, multiplier: 1, constant: 0).isActive = true
+        }
 
+        if let configureImageViewCustom = configureImageViewCustom {
+            configureImageViewCustom((sub, index))
+            return
+        }
+        
         let iv = UIImageView()
-        iv.layer.cornerRadius = bounds.height / 2
         iv.clipsToBounds = true
         iv.translatesAutoresizingMaskIntoConstraints = false
         iv.backgroundColor = getRandomColor(index: index)
@@ -188,17 +240,29 @@ public class ProfileClusterView: UIView {
         configureImageView?((iv, index))
     }
 
-    func addLabel(more: Int, index: Int) {
+    private func addLabel(more: Int, index: Int) {
         let sub = UIView()
         sub.translatesAutoresizingMaskIntoConstraints = false
-        sub.heightAnchor.constraint(equalTo: sub.widthAnchor, multiplier: 1, constant: 0).isActive = true
         stackView.addArrangedSubview(sub)
+        if let itemWidth = itemWidth {
+            sub.heightAnchor.constraint(equalTo: stackView.heightAnchor, multiplier: 1, constant: 0).isActive = true
+            sub.widthAnchor.constraint(equalToConstant: itemWidth).isActive = true
+        } else {
+            sub.heightAnchor.constraint(equalTo: sub.widthAnchor, multiplier: 1, constant: 0).isActive = true
+        }
+        
+        if let configureMoreViewCustom = configureMoreViewCustom {
+            configureMoreViewCustom((sub, more))
+            return
+        }
 
         let lbl = UILabel()
         lbl.translatesAutoresizingMaskIntoConstraints = false
-        lbl.layer.cornerRadius = bounds.height / 2
-        lbl.font = UIFont.boldSystemFont(ofSize: bounds.height / 2.5)
+        lbl.font = UIFont.boldSystemFont(ofSize: getItemWidth() / 2.5)
         lbl.numberOfLines = 1
+        lbl.baselineAdjustment = .alignCenters
+        lbl.adjustsFontSizeToFitWidth = true
+        lbl.lineBreakMode = .byClipping
         lbl.textAlignment = .center
         lbl.clipsToBounds = true
         lbl.textColor = .white
@@ -210,14 +274,14 @@ public class ProfileClusterView: UIView {
         configureMoreView?((lbl, more))
     }
 
-    func getRandomColor(index: Int) -> UIColor {
+    private func getRandomColor(index: Int) -> UIColor {
         let color1 = UIColor(red: 179 / 255, green: 235 / 255, blue: 242 / 255, alpha: 1)
         let color2 = UIColor(red: 239 / 255, green: 154 / 255, blue: 154 / 255, alpha: 1)
         let color3 = UIColor(red: 165 / 255, green: 214 / 255, blue: 167 / 255, alpha: 1)
         return index % 3 == 0 ? color1 : (index % 2 == 0 ? color2 : color3)
     }
 
-    func getLabelColor() -> UIColor {
+    private func getLabelColor() -> UIColor {
         return UIColor(red: 126 / 255, green: 87 / 255, blue: 194 / 255, alpha: 1)
     }
 
@@ -227,7 +291,7 @@ public class ProfileClusterView: UIView {
         configureMoreView = viewProfiles.configureMoreView
     }
 
-    func addPinConstraints(view: UIView, val: CGFloat) {
+    private func addPinConstraints(view: UIView, val: CGFloat) {
         view.translatesAutoresizingMaskIntoConstraints = false
         view.leadingAnchor.constraint(equalTo: view.superview!.leadingAnchor, constant: val).isActive = true
         view.trailingAnchor.constraint(equalTo: view.superview!.trailingAnchor, constant: -val).isActive = true
@@ -235,7 +299,7 @@ public class ProfileClusterView: UIView {
         view.bottomAnchor.constraint(equalTo: view.superview!.bottomAnchor, constant: -val).isActive = true
     }
     
-    func setShadowLevel(_ shadowLevel: CGFloat, layer: CALayer? = nil) {
+    private func setShadowLevel(_ shadowLevel: CGFloat, layer: CALayer? = nil) {
         let layer = layer ?? self.layer
         layer.shadowColor = UIColor.gray.cgColor
         layer.shadowOffset = CGSize(width: shadowLevel * 0.5, height: shadowLevel * 0.5)
